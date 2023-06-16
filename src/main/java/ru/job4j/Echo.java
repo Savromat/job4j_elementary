a0cb4bca6667cf8c8c9dfce46fa5c86e0d34327e
Pavel <paleon@mail.ru>
2023-06-16 22:33:34 +0300

    3.2. Загрузка файлов на github [#243562 #204864]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -4,5 +4,6 @@ public class Echo {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(1);
+        System.out.println(2);
     }
 }
