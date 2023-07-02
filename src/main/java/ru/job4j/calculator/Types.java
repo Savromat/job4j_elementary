b9eb47fdcd5d78bde6f18c575fa10966f6ae0dd3
Pavel <paleon@mail.ru>
2023-07-02 15:00:00 +0300

    1. Модульные тесты. [#159435 #426949]

---
+++ src/main/java/ru/job4j/calculator/Types.java
@@ -0,0 +1,41 @@
+package ru.job4j.calculator;
+
+public class Types {
+
+    public static void main(String[] args) {
+        /*
+        int num = 2_147_483_600;
+        float f = num;
+
+        int i1 = 10000;
+        byte b1 = (byte) i1;
+        short s1 = (short) i1;
+        System.out.println("Вывод в консоль значения после преобразования: " + b1);
+        System.out.println("Вывод в консоль значения после преобразования: " + s1);
+
+        double d2 = 94.984751;
+        int i2 = (int) d2;
+        int i3 = (int) Math.round(d2);
+        System.out.println("Вывод в консоль значения после преобразования: " + i2);
+        System.out.println("Вывод в консоль значения после преобразования: " + i3);
+
+        int i5 = 100;
+        char c = (char) i5;
+        System.out.println("Вывод в консоль значения после преобразования: " + c);
+
+        int i6 = 100;
+        double d6 = 85.6;
+        double rsl = i6 + d6;
+        System.out.println("Результат вычисления приведен к double: " + rsl);
+
+        int rsl1 = 100 + 's';
+        System.out.println("Результат вычисления c типом char - преобразован к типу int: " + rsl1);
+        */
+        char a = 'A';
+        char b = 'B';
+        System.out.println(a + b);
+        char d = 'D';
+        System.out.println(++a);
+        System.out.println(--d);
+    }
+}
