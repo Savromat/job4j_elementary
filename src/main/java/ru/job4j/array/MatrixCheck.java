89642c1b1fb82990d05452b9794d35c27a9969bf
Pavel <paleon@mail.ru>
2023-09-12 13:49:11 +0300

    6.7.1. Моно строка в матрице. [#214126]

---
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] == ' ') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[][] array = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', 'X', ' '},
+        };
+        System.out.println(MatrixCheck.monoHorizontal(array, 1));
+    }
+}
---
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class MatrixCheckTest {
+
+    @Test
+    void whenHasMonoHorizontalTrue() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenHasMonoHorizontalFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 2;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenHasNotMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {'X', 'X', ' '},
+                {' ', 'X', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isFalse();
+    }
+}
