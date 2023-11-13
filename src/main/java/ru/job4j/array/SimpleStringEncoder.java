f433daf452c2d993151523b82c71c4a381e5c208
Pavel <paleon@mail.ru>
2023-11-13 22:38:10 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049 #459473]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -23,7 +23,7 @@ public class SimpleStringEncoder {
     public static void main(String[] args) {
         String first = "aaaabbbccd";
         String second = "abc";
-        String third = "aaa";
+        String third = "aa";
         System.out.println(encode(first));
         System.out.println(encode(second));
         System.out.println(encode(third));
