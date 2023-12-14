d961c6215ca730e3b4743e6ef424429f7ef911b6
Pavel <paleon@mail.ru>
2023-12-14 19:07:27 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,7 +6,17 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int right, int high) {
+        return high > max(left, right) ? high : max(left, right);
+    }
+
+    public static int max(int left, int right, int high, int bottom) {
+        return max(high, bottom) > max(left, right) ? max(high, bottom) : max(left, right);
+    }
+
     public static void main(String[] args) {
         System.out.println(Max.max(5, 4));
+        System.out.println(Max.max(5, 4, 6));
+        System.out.println(Max.max(5, 4, 6, 7));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,46 @@ class MaxTest {
         int expected = 8;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax1To3Then1O() {
+        int left = 8;
+        int right = 9;
+        int high = 10;
+        int result = Max.max(left, right, high);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax2To3Then9() {
+        int left = 9;
+        int right = 9;
+        int high = 5;
+        int result = Max.max(left, right, high);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1To4Then1O() {
+        int left = 8;
+        int right = 9;
+        int high = 10;
+        int bottom = 5;
+        int result = Max.max(left, right, high, bottom);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax2To4Then9() {
+        int left = 8;
+        int right = 9;
+        int high = 9;
+        int bottom = 5;
+        int result = Max.max(left, right, high, bottom);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
 }
